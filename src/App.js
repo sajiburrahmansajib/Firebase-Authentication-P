aab49f5e9a05c2cfe665693e953d3cefd8c0ac60
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-10-15 23:52:55 +0600

    Daisyui navbar added & initial setup complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Main from './Layout/Main';
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3>Please Log in !!!!!</h3>
+        </div>
+    );
+};
+
+export default Login;
